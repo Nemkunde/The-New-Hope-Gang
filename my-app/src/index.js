f592fb3c98e48304240a8819efa4e29968adb21e
Nemkunde <113009899+Nemkunde@users.noreply.github.com>
2023-01-25 14:29:34 +0100

    Changed names and added react router

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,9 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import HomePage from "./Components/HomePage";
+import CharacterList from "./Components/CharacterList";
+import PlanetList from "./Components/PlanetList";
+import SpeciesList from "./Components/SpeciesList";
+import StarshipList from "./Components/StarshipList";
+import TitleList from "./Components/TitleList";
+import VehicleList from "./Components/VehicleList";
+import NoPage from ".Components/NoPage";
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+
+
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route>
+          <Route index element={<HomePage />} />
+          <Route path="Characters" element={<CharacterList />} />
+          <Route path="Planets" element={<PlanetList />} />
+          <Route path="Species" element={<SpeciesList />} />
+          <Route path="Starships" element={<StarshipList />} />
+          <Route path="Titles" element={<TitleList />} />
+          <Route path="Vehicles" element={<VehicleList />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -11,7 +39,5 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
