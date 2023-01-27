8e0749c554f32507179ade57d802c196ac31c982
PontusEkstrom02 <113009598+PontusEkstrom02@users.noreply.github.com>
2023-01-27 11:34:21 +0100

    Update CharacterList.js

--- my-app/src/pages/CharacterList.js
+++ my-app/src/pages/CharacterList.js
@@ -1,21 +1,58 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-let carname;
+let charNamesList = [];
+let tableNum = 1; // 1 6 11 16 21 26 31
+let listAmount;
 const CharacterList = () => {
-    fetch('https://swapi.dev/api/people/1/')
-        .then(response => response.json())
-        .then(json => {
-            carname = json.name;
+    fetch('https://swapi.dev/api/people/')
+    .then(response => response.json())
+    .then(json => {
+        listAmount = json.count;
     })
     .catch(error => {
       console.error(error);
     });
+    for (let i = 0; i < listAmount; i++) {
+        fetch('https://swapi.dev/api/people/' + i)
+         .then(response => response.json())
+         .then(json => {
+            charNamesList[i] = json.name;
+        })
+        .catch(error => {
+        console.error(error);
+        });
+    }
+    function NextPage(){
+        if(tableNum < listAmount-5){
+            tableNum = tableNum + 5
+        }
+        else{
+            tableNum = 1
+        }
+    }
+    function PrevPage(){
+        if(tableNum > 5){
+            tableNum = tableNum - 5
+        }
+        else{
+            tableNum = listAmount-5
+        }
+    }
     return (
         <>
             <h1>Characters</h1>
-            <Link to={"/CharacterList/" + carname}>{carname}</Link>
+            <Link to={"/CharacterList/" + charNamesList[tableNum]}>{charNamesList[tableNum]}</Link>
             <br />
-            <Link to="/CharacterList/Joda">Joda</Link>
+            <Link to={"/CharacterList/" + charNamesList[tableNum+1]}>{charNamesList[tableNum+1]}</Link>
+            <br />
+            <Link to={"/CharacterList/" + charNamesList[tableNum+2]}>{charNamesList[tableNum+2]}</Link>
+            <br />
+            <Link to={"/CharacterList/" + charNamesList[tableNum+3]}>{charNamesList[tableNum+3]}</Link>
+            <br />
+            <Link to={"/CharacterList/" + charNamesList[tableNum+4]}>{charNamesList[tableNum+4]}</Link>
+            <br />
+            <button onClick={NextPage}>prev</button>
+            <button onClick={PrevPage}>next</button>
             <br />
             <br />
             <Link to="../">Back</Link>
