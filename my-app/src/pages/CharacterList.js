a5fd46ce44d23c6c7014d97f6408ad12760dc2ac
PontusEkstrom02 <113009598+PontusEkstrom02@users.noreply.github.com>
2023-01-27 15:24:56 +0100

    fixed all

--- my-app/src/pages/CharacterList.js
+++ my-app/src/pages/CharacterList.js
@@ -1,58 +1,55 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-let charNamesList = [];
-let tableNum = 1; // 1 6 11 16 21 26 31
-let listAmount;
+let peopleInfoList = [];
+let names = []
 const CharacterList = () => {
     fetch('https://swapi.dev/api/people/')
-    .then(response => response.json())
-    .then(json => {
-        listAmount = json.count;
+        .then(response => response.json())
+        .then(json => {
+            peopleInfoList = json.results
+            console.log(peopleInfoList)
+            for (let i = 0; i < peopleInfoList.length; i++) {
+                names[i] = peopleInfoList[i].name
+            }
     })
     .catch(error => {
-      console.error(error);
+    console.error(error);
     });
-    for (let i = 0; i < listAmount; i++) {
-        fetch('https://swapi.dev/api/people/' + i)
-         .then(response => response.json())
-         .then(json => {
-            charNamesList[i] = json.name;
-        })
-        .catch(error => {
-        console.error(error);
-        });
-    }
-    function NextPage(){
-        if(tableNum < listAmount-5){
-            tableNum = tableNum + 5
-        }
-        else{
-            tableNum = 1
-        }
-    }
-    function PrevPage(){
-        if(tableNum > 5){
-            tableNum = tableNum - 5
-        }
-        else{
-            tableNum = listAmount-5
-        }
-    }
     return (
         <>
             <h1>Characters</h1>
-            <Link to={"/CharacterList/" + charNamesList[tableNum]}>{charNamesList[tableNum]}</Link>
-            <br />
-            <Link to={"/CharacterList/" + charNamesList[tableNum+1]}>{charNamesList[tableNum+1]}</Link>
-            <br />
-            <Link to={"/CharacterList/" + charNamesList[tableNum+2]}>{charNamesList[tableNum+2]}</Link>
-            <br />
-            <Link to={"/CharacterList/" + charNamesList[tableNum+3]}>{charNamesList[tableNum+3]}</Link>
-            <br />
-            <Link to={"/CharacterList/" + charNamesList[tableNum+4]}>{charNamesList[tableNum+4]}</Link>
-            <br />
-            <button onClick={PrevPage}>prev</button>
-            <button onClick={NextPage}>next</button>
+            <ul>
+                <li>
+                    <Link to={"/CharacterList/" + 1}>{names[0]}</Link>
+                </li>
+                <li>
+                    <Link to={"/CharacterList/" + 2}>{names[1]}</Link>
+                </li>
+                <li>
+                    <Link to={"/CharacterList/" + 3}>{names[2]}</Link>
+                </li>
+                <li>
+                    <Link to={"/CharacterList/" + 4}>{names[3]}</Link>
+                </li>
+                <li>
+                    <Link to={"/CharacterList/" + 5}>{names[4]}</Link>
+                </li>
+                <li>
+                    <Link to={"/CharacterList/" + 6}>{names[5]}</Link>
+                </li>
+                <li>
+                    <Link to={"/CharacterList/" + 7}>{names[6]}</Link>
+                </li>
+                <li>
+                    <Link to={"/CharacterList/" + 8}>{names[7]}</Link>
+                </li>
+                <li>
+                    <Link to={"/CharacterList/" + 9}>{names[8]}</Link>
+                </li>
+                <li>
+                    <Link to={"/CharacterList/" + 10}>{names[9]}</Link>
+                </li>
+            </ul>
             <br />
             <br />
             <Link to="../">Back</Link>
--- my-app/src/pages/Characters.js
+++ my-app/src/pages/Characters.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import { Link, useParams } from 'react-router-dom';
+let names, birth_year, eye;
 export default function Characters(){
     const { id } = useParams();
+    fetch('https://swapi.dev/api/people/' + id)
+        .then(response => response.json())
+        .then(json => {
+            names = json.name;
+            birth_year = json.birth_year;
+            eye = json.eye_color;
+    })
     return (
         <>
-        <h1>wow! its {id}</h1>
+        <ul>
+            <li>
+                <p>namn: {names}</p>
+            </li>
+            <li>
+                <p>birth_year: {birth_year}</p>
+            </li>
+            <li>
+                <p>eye: {eye}</p>
+            </li>
+        </ul>
         <Link to="../CharacterList">Back</Link>
         </>
     );
